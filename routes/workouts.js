dbd76b0ed8552df607c41015d66049ad7b05cf18
MorganHuegel <mhue2910@gmail.com>
2018-08-10 17:18:22 -0400

    created a PUT endpoint for /workouts.  It replaces the entire workout completely, not just update

--- routes/workouts.js
+++ routes/workouts.js
@@ -74,23 +74,48 @@ workoutRouter.delete('/:id', verifyTokenMiddleware, (req, res, next) => {
 
 workoutRouter.post('/', verifyTokenMiddleware, (req, res, next) => {
   const username = req.username;
-  let workoutId;
+  let dbNewWorkout;
 
   const newWorkout = req.body; //userId and date should be appended to request body on the client-side
   Workout.create(newWorkout) //creates in workout collection
     .then(result => {
-      workoutId = result.id;
+      dbNewWorkout = result;
       return User.findOneAndUpdate(   //grabs new id from response and adds it to workout array in Users
         {username},
-        {$push: {workouts: workoutId}}
+        {$push: {workouts: dbNewWorkout.id}}
       );
     })
     .then( () => {
-      return res.json({id: workoutId});  //sends the new id back, where client-side appends it to newWorkout Object before updating redux state
+      return res.json(dbNewWorkout);  //sends the new workout back, where cliet-side can append to redux state
     })
     .catch(err => {
       next(err);
     });
+});
+
+
+
+workoutRouter.put('/', verifyTokenMiddleware, (req, res, next) => {
+  const username = req.username;
+  const workoutUpdate = req.body;
+
+  return User.findOne({_id: workoutUpdate.userId, username}) //verifies that the workout belongs to the user with the token
+    .then(result => {
+      if(!result){
+        const err = new Error('Not authorized to edit this workout');
+        err.status = 401;
+        return next(err);
+      } else {
+        return Workout.replaceOne({_id: workoutUpdate.id}, workoutUpdate);
+      }
+    })
+    .then( () => {
+      return Workout.findOne({_id: workoutUpdate.id});
+    })
+    .then( updatedWorkout => {
+      return res.json(updatedWorkout);
+    })
+    .catch(err => next(err));
 });
 
 
